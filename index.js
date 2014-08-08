dbb9d45904cbb71761e16acf8dc47e07eae81c61
Michael Ridgway <mcridgway@gmail.com>
2014-08-08 16:08:37 -0700

    Fix casing

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
  * Copyright 2014, Yahoo! Inc.
  * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
  */
-module.exports = require('./lib/dispatcher');
+module.exports = require('./lib/Dispatcher');
